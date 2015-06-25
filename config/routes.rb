a519b7f375c048229b4859160e8159ffa46bc8dd
niconan <nick.hax@gmail.com>
2015-06-24 19:09:50 -0700

    scaffolded users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   root 'static_pages#home'
   StaticPagesController.action_methods.each do |action|
     get "/#{action}", to: "static_pages##{action}", as: "#{action}_page"
