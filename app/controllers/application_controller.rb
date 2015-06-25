6664d00e576f99715982c956feaa07448a7da674
niconan <nick.hax@gmail.com>
2015-06-24 21:33:40 -0700

    navbar changes on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,4 +26,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
   end
   helper_method :current_user
+
+  def signed_in_user?
+    !!current_user
+  end
+  helper_method :signed_in_user?
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      if params[:remember_me]
+        permanent_sign_in(@user)
+      else
+        sign_in(@user)
+      end
+      flash[:success] = "Welcome, #{@user.first_name}!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Couldn't sign you in!"
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:success] = "See you, space cowboy..."
+    redirect_to root_path
+  end
 end
