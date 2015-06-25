1accd279fc6e9e214427c992ab5a9af2b14f9d4c
niconan <nick.hax@gmail.com>
2015-06-24 19:53:01 -0700

    db issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
 
-  root 'static_pages#home'
+  root 'users#new'
   StaticPagesController.action_methods.each do |action|
     get "/#{action}", to: "static_pages##{action}", as: "#{action}_page"
   end
--- db/migrate/20150625020702_create_users.rb
+++ db/migrate/20150625020702_create_users.rb
@@ -4,8 +4,14 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.string :password_digest
+      t.string :auth_token
 
       t.timestamps null: false
     end
+
+    add_index :users, :email, unique: true
+    add_index :users, :password_digest, unique: true
+    add_index :users, :auth_token, unique: true
   end
 end
